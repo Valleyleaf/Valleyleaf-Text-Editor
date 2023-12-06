a603a11555010d1dfcfef06b84eb6731222db1c5
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-12-06 12:47:51 -0400

    Added some base code from mini-project. working on refactoring and then testing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,8 +15,13 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
         console.error('putDb not implemented', content)
-        const valleyLeafDb = await openDB('valleyleaf', 1)
-
+        const jateDB = await openDB('jate', 1);
+        const tx = jateDB.transaction('jate', 'readwrite');
+        const store = tx.objectStore('jate');
+        const request = store.put({ id: 1, text: content });
+        const result = await request;
+        console.log('RESULT ON PUT IS:', result);
+        return result;
 };
 
 
@@ -25,7 +30,13 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented')
-
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('RESULT ON GET IS:', result);
+  return result;
 };
 
 initdb();
